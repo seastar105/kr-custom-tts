ddfe12131eee28a3c89d1ad147144b35ec3e6e5b
seastar105 <seastar105@gmail.com>
2022-06-10 13:51:40 +0900

    Remove pipefail

--- tts1/local/trim_silence.sh
+++ tts1/local/trim_silence.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-set -u
-set -o pipefail
-
 # trim silence at start and end using kaldi's script
 fs=24000
 for dset in "train" "eval";
@@ -11,7 +7,7 @@ do
     datadir=data/${dset}
     scripts/audio/trim_silence.sh \
         --fs ${fs} \
-        ${datadir} ${datadir}/log
+        ${datadir} ${datadir}/log 
     # fix dset since segments file added
     utils/fix_data_dir.sh ${datadir}
 done
