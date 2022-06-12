0177a2d9f7cfce298bd55d048bdd85dccfb0f4d0
seastar105 <seastar105@gmail.com>
2022-06-12 12:23:28 +0900

    Update transcripts.txt

--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-
-# build image
-docker build -t jets-finetune .
-docker run -it --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -p 6006:6006 -p 8000:8000 -v $(pwd):/root/espnet/egs2/finetune jets-finetune bash
+docker run -it --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -p 6006:6006 -p 8888:8888 -v $(pwd):/root/espnet/egs2/finetune seastar105/jets-finetune bash
